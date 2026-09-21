0c95414f66b605492247f02f92864793203ce8b3
nishi kant <nkant27@gmail.com>
2026-09-21 16:24:35 +0530

    df

--- main.tf
+++ main.tf
@@ -1,22 +1,10 @@
 module "my_rg" {
     source = "../../modules/azure_resourcegroup"
-    rgs = {
-     my_module_rg = "Central India"
-    }
+    rgs = var.rgs
 }
 
 module "my_storage" {
     depends_on = [ module.my_rg ]
     source = "../../modules/azure_storage_account"
-    storage ={
-  sg1 = {
-  name                     = "sto0079"
-  resource_group_name      = "my_module_rg"
-  location                 = "Central India"
-  account_tier             = "Standard"
-  account_replication_type = "GRS"
-  
- }
- }
-}
-
+    storage = var.storage
+    }
---
+++ terraform.tfvars
@@ -0,0 +1,14 @@
+rgs = {
+     vistara_rg = "Central India"
+    }
+
+
+storage = {
+    stg1 = {
+        name                     = "vistarastg789"
+        resource_group_name      = "vistara_rg"
+        location                 = "Central India"
+        account_tier             = "Standard"
+        account_replication_type = "LRS"  
+        }
+    }
